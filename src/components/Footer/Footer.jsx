00ab0861905b578fe0e19671c8c900e932bf1cab
Dũng Prj <89343958+Dungprj@users.noreply.github.com>
2025-03-30 21:13:59 +0700

    Add src/components/Footer/Footer.jsx 1743344038486

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,20 @@
 import './Footer.scss';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="container">
         <div className="footer__content">
+          <div className="footer__section">
+            <h3 className="footer__title">Điều hướng</h3>
+            <div className="footer__links">
+              <Link to="/" className="footer__link">Trang chủ</Link>
+              <Link to="/products" className="footer__link">Sản phẩm</Link>
+              <Link to="/contact" className="footer__link">Liên hệ</Link>
+            </div>
+          </div>
+
           <div className="footer__section">
             <h3 className="footer__title">Về chúng tôi</h3>
             <p className="footer__text">
@@ -28,13 +38,13 @@ const Footer = () => {
           <div className="footer__section">
             <h3 className="footer__title">Mạng xã hội</h3>
             <div className="footer__social">
-              <a href="#" className="footer__social-link">
+              <a href="https://facebook.com" className="footer__social-link" target="_blank" rel="noopener noreferrer">
                 <i className="fab fa-facebook-f"></i>
               </a>
-              <a href="#" className="footer__social-link">
+              <a href="https://youtube.com" className="footer__social-link" target="_blank" rel="noopener noreferrer">
                 <i className="fab fa-youtube"></i>
               </a>
-              <a href="#" className="footer__social-link">
+              <a href="https://zalo.me" className="footer__social-link" target="_blank" rel="noopener noreferrer">
                 <i className="fab fa-zalo"></i>
               </a>
             </div>
